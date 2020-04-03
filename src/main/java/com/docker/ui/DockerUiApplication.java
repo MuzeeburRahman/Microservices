b12a4cec0ee5a2d32c60c7cb99120ac3de2b489f
muzeeb <muzeeb@localhost>
2020-04-04 02:33:14 +0530

    first commit

---
+++ src/main/java/com/docker/ui/DockerUiApplication.java
@@ -0,0 +1,13 @@
+package com.docker.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerUiApplication.class, args);
+	}
+
+}
